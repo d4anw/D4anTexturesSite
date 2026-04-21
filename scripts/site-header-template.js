37ce3501c97ca951229aeb55b07b62fffdb6701c
Daan de Weerd <d.deweerd02@student.alfa-college.nl>
2026-04-21 10:54:58 +0200

    Refactor texture pack pages and update showcase links

--- scripts/site-header-template.js
+++ scripts/site-header-template.js
@@ -1,12 +1,12 @@
 window.SITE_HEADER_TEMPLATE = [
     '<header class="site-header">',
     '    <div class="container nav-wrap">',
-    '        <a class="brand" data-path="index.html" href="#">D4anTextures</a>',
+    '        <a class="brand" data-path="" href="#">D4anTextures</a>',
     '        <nav class="main-nav" aria-label="Main navigation">',
-    '            <a data-nav="home" data-path="index.html" href="#">Home</a>',
-    '            <a data-nav="showcase" data-path="pages/showcase/showcase.html" href="#">Showcase</a>',
-    '            <a data-nav="how-to-download" data-path="pages/info/how-to-download.html" href="#">How To Download</a>',
-    '            <a data-nav="socials" data-path="pages/info/socials.html" href="#">Socials</a>',
+    '            <a data-nav="home" data-path="" href="#">Home</a>',
+    '            <a data-nav="showcase" data-path="pages/showcase/overview" href="#">Showcase</a>',
+    '            <a data-nav="how-to-download" data-path="pages/info/how-to-download" href="#">How To Download</a>',
+    '            <a data-nav="socials" data-path="pages/info/socials" href="#">Socials</a>',
     '        </nav>',
     '    </div>',
     '</header>'
