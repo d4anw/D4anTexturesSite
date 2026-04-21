e73b0d1b61cb680cfeb1dc2a61ac6774e54810f6
d4anw <daandeweerd2009@gmail.com>
2026-04-21 13:22:55 +0200

    Fix GitHub Pages redirect loop on showcase/overview - delete old redirect file and add trailing slashes

--- scripts/site-header-template.js
+++ scripts/site-header-template.js
@@ -4,7 +4,7 @@ window.SITE_HEADER_TEMPLATE = [
     '        <a class="brand" data-path="" href="#">D4anTextures</a>',
     '        <nav class="main-nav" aria-label="Main navigation">',
     '            <a data-nav="home" data-path="" href="#">Home</a>',
-    '            <a data-nav="showcase" data-path="pages/showcase/overview" href="#">Showcase</a>',
+    '            <a data-nav="showcase" data-path="pages/showcase/overview/" href="#">Showcase</a>',
     '            <a data-nav="how-to-download" data-path="pages/info/how-to-download" href="#">How To Download</a>',
     '            <a data-nav="socials" data-path="pages/info/socials" href="#">Socials</a>',
     '        </nav>',
